988669001a550b025f109eb5d3dab6668c943c78
Y A S <54225481+specialyas@users.noreply.github.com>
2024-07-27 15:36:17 +0100

    log user data from client

--- api/app.js
+++ api/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import postRoute from "./routes/post.routes.js";
 import authRoute from "./routes/auth.route.js";
+
 
 const app = express();
 
 // middlewares
 app.use(express.json())
+app.use(cors({origin: process.env.CLIENT_URL, credentials:true}))
 app.use(cookieParser())
 
 app.use("/api/posts", postRoute);
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -60,6 +60,8 @@ export const login = async (req, res) => {
       { expiresIn: age }
     );
 
+    const {password: userPassword, ...userInfo} = user
+
     res
       .cookie("token", token, {
         httpOnly: true,
@@ -67,7 +69,7 @@ export const login = async (req, res) => {
         maxAge: age,
       })
       .status(200)
-      .json({ message: "Login Successful" });
+      .json(userInfo);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Login unsucessfull !!!" });
